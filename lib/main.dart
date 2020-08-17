3e27cc2b22aeb6a5097007a200a1d0e2ca10552b
michaelbrinkworth <michaelbrinkworth@users.noreply.github.com>
2020-08-17 10:17:57 +0300

    update
    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:jitsi_meet/jitsi_meet.dart';
-import 'package:jitsi_meet/jitsi_meeting_listener.dart';
-import 'package:jitsi_meet/room_name_constraint.dart';
-import 'package:jitsi_meet/room_name_constraint_type.dart';
-import 'package:jitsi_meet/feature_flag/feature_flag_enum.dart';
+import 'package:jitsi_meet_example/setting.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,258 +12,71 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final serverText = TextEditingController(text: "https://meet.jit.si");
-  final roomText = TextEditingController(text: "salim");
-  final subjectText = TextEditingController(text: "Guid.fm for Speaker");
-  final nameText = TextEditingController(text: "Speaker1");
-  final emailText = TextEditingController(text: "speaker@gmail.com");
-  final iosAppBarRGBAColor = TextEditingController(text: "#0080FF80");//transparent blue
-  var isAudioOnly = true;
-  var isAudioMuted = true;
-  var isVideoMuted = true;
-
-  @override
-  void initState() {
-    super.initState();
-    JitsiMeet.addListener(JitsiMeetingListener(
-        onConferenceWillJoin: _onConferenceWillJoin,
-        onConferenceJoined: _onConferenceJoined,
-        onConferenceTerminated: _onConferenceTerminated,
-        onError: _onError));
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    JitsiMeet.removeAllListeners();
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 24.0,
-                ),
-                TextField(
-                  controller: serverText,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Server URL",
-                      hintText: "Hint: Leave empty for https://meet.jit.si"),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                TextField(
-                  controller: roomText,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Room",
-                  ),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                TextField(
-                  controller: subjectText,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Subject",
-                  ),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                TextField(
-                  controller: nameText,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Display Name",
-                  ),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                TextField(
-                  controller: emailText,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Email",
-                  ),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                TextField(
-                  controller: iosAppBarRGBAColor,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "AppBar Color(IOS only)",
-                      hintText: "Hint: This HAS to be in HEX RGBA format"),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                CheckboxListTile(
-                  title: Text("Audio Only"),
-                  value: isAudioOnly,
-                  onChanged: _onAudioOnlyChanged,
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                CheckboxListTile(
-                  title: Text("Audio Muted"),
-                  value: isAudioMuted,
-                  onChanged: _onAudioMutedChanged,
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                CheckboxListTile(
-                  title: Text("Video Muted"),
-                  value: isVideoMuted,
-                  onChanged: _onVideoMutedChanged,
-                ),
-                Divider(
-                  height: 48.0,
-                  thickness: 2.0,
-                ),
-                SizedBox(
-                  height: 64.0,
-                  width: double.maxFinite,
-                  child: RaisedButton(
-                    onPressed: () {
-                      _joinMeeting();
-                    },
-                    child: Text(
-                      "Join Meeting",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    color: Colors.blue,
-                  ),
-                ),
-                SizedBox(
-                  height: 48.0,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Home()
     );
   }
+}
 
-  _onAudioOnlyChanged(bool value) {
-    setState(() {
-      isAudioOnly = value;
-    });
-  }
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-  _onAudioMutedChanged(bool value) {
-    setState(() {
-      isAudioMuted = value;
-    });
-  }
+class _HomeState extends State<Home> {
 
-  _onVideoMutedChanged(bool value) {
-    setState(() {
-      isVideoMuted = value;
-    });
-  }
-
-  _joinMeeting() async {
-    String serverUrl = "https://meet.jit.si";
-        //serverText.text?.trim()?.isEmpty ?? "" ? null : serverText.text;
-
-    try {
-
-      // Enable or disable any feature flag here
-      // If feature flag are not provided, default values will be used
-      // Full list of feature flags (and defaults) available in the README
-      Map<FeatureFlagEnum, bool> featureFlags =
-      {
-        FeatureFlagEnum.WELCOME_PAGE_ENABLED : false,
-      };
-
-      // Here is an example, disabling features for each platform
-      if (Platform.isAndroid)
-      {
-        // Disable ConnectionService usage on Android to avoid issues (see README)
-        featureFlags[FeatureFlagEnum.CALL_INTEGRATION_ENABLED] = false;
-      }
-      else if (Platform.isIOS)
-      {
-        // Disable PIP on iOS as it looks weird
-        featureFlags[FeatureFlagEnum.PIP_ENABLED] = false;
-      }
-
-      // Define meetings options here
-      var options = JitsiMeetingOptions()
-        ..room = roomText.text
-        ..serverURL = serverUrl
-        ..subject = subjectText.text
-        ..userDisplayName = nameText.text
-        ..userEmail = emailText.text
-        ..iosAppBarRGBAColor = iosAppBarRGBAColor.text
-        ..audioOnly = isAudioOnly
-        ..audioMuted = isAudioMuted
-        ..videoMuted = isVideoMuted
-        ..featureFlags.addAll(featureFlags);
-
-      debugPrint("JitsiMeetingOptions: $options");
-      await JitsiMeet.joinMeeting(options,
-          listener: JitsiMeetingListener(onConferenceWillJoin: ({message}) {
-            debugPrint("${options.room} will join with message: $message");
-          }, onConferenceJoined: ({message}) {
-            debugPrint("${options.room} joined with message: $message");
-          }, onConferenceTerminated: ({message}) {
-            debugPrint("${options.room} terminated with message: $message");
-          }),
-          // by default, plugin default constraints are used
-          //roomNameConstraints: new Map(), // to disable all constraints
-          //roomNameConstraints: customContraints, // to use your own constraint(s)
-      );
-    } catch (error) {
-      debugPrint("error: $error");
-    }
-  }
-
-  static final Map<RoomNameConstraintType, RoomNameConstraint> customContraints =
-  {
-    RoomNameConstraintType.MAX_LENGTH : new RoomNameConstraint(
-            (value) { return value.trim().length <= 50; },
-            "Maximum room name length should be 30."),
-
-    RoomNameConstraintType.FORBIDDEN_CHARS : new RoomNameConstraint(
-            (value) { return RegExp(r"[$€£]+", caseSensitive: false, multiLine: false).hasMatch(value) == false; },
-            "Currencies characters aren't allowed in room names."),
-  };
-
-  void _onConferenceWillJoin({message}) {
-    debugPrint("_onConferenceWillJoin broadcasted with message: $message");
-  }
-
-  void _onConferenceJoined({message}) {
-    debugPrint("_onConferenceJoined broadcasted with message: $message");
-  }
-
-  void _onConferenceTerminated({message}) {
-    debugPrint("_onConferenceTerminated broadcasted with message: $message");
-  }
-
-  _onError(error) {
-    debugPrint("_onError broadcasted: $error");
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Audio Broadcast'),
+        actions: [
+          Container(
+            height: MediaQuery.of(context).padding.top,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(right:20.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => Settings(),
+                ));
+              },
+              child: Text('Setting', style: TextStyle(fontSize: 15)),
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          children: [
+            SizedBox(height: 100,),
+            Text(
+              'Guid.fm for Speaker',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500
+              ),
+            ),
+            SizedBox(height: 30,),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Channel Name'
+              ),
+            ),
+            SizedBox(height: 20,),
+            RaisedButton(
+              onPressed: () {},
+              child: const Text('Create Channel', style: TextStyle(fontSize: 16)),
+            ),
+          ],
+        )
+      ),
+    );
   }
 }
